493313605594e03a9d4461d6a16b78491fe345e6
Ching Ching Lee <allisonchinglee@gmail.com>
2018-12-17 22:48:42 -0700

    adds method

--- src/components/RestaurantList/RestaurantList.js
+++ src/components/RestaurantList/RestaurantList.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux/Aux';
+import classes from './RestaurantList.css';
 
 const restaurantList = (props) => {
     return(
         <Aux>
-            <button onClick={props.getList}>Get List</button>
+            <button className={classes.testing} onClick={props.getList}>Get List</button>
             <ul>
                 { props.rList }
             </ul>
